f02a8e4bcbe619d87f8c83581ced24ee52259668
Patrik Stutz <patrik.stutz@gmail.com>
2024-09-30 00:24:46 +0200

    make process-compose-flake usable without flake-parts

--- example/flake.nix
+++ example/flake.nix
@@ -76,6 +76,25 @@
               };
             };
           };
+
+        # nix run .#ponysay up to start the process
+        packages.ponysay = (import ../nix/eval-modules.nix).makeProcessCompose {
+          inherit pkgs;
+          name = "ponysay";
+          modules = [{
+            arguments.detached = true;
+            settings = {
+              processes = {
+                ponysay.command = ''
+                  while true; do
+                    ${lib.getExe pkgs.ponysay} "Hi!"
+                    sleep 2
+                  done
+                '';
+              };
+            };
+          }];
+        };
       };
     };
 }
